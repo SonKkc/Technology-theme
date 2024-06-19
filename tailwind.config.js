9ea38666a76b6934a5ef04e1c05e6d5c027bac10
SonKkc <soncb.xyz@gmail.com>
2024-06-19 10:58:58 +0700

    Home page update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["*"],
+  content: ["*"], // Bạn có thể thay "*" bằng các đường dẫn cụ thể nếu cần
   theme: {
     extend: {},
+    screens: {
+      'sm': '640px',
+      'tablet': '640px',
+      'laptop': '1024px',
+      'desktop': '1280px',
+    },
   },
   plugins: [],
 }
-
