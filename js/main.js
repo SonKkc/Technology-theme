8ecb332acbd1658fcdce073cd22fe319f9f3d635
SonKkc <soncb.xyz@gmail.com>
2024-06-22 16:03:16 +0700

    category page 2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-document.addEventListiener('DOMContentLoaded', () => {
-    console.log('Document loaded!');
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Document is ready');
+    const searchButton = document.querySelector('.page_name-n-search .-btn');
+    const searchBar = document.querySelector('.page_name-n-search .-searchbar');
+    const pageTitle = document.querySelector('.page_name-n-search .-title');
+
+    searchButton.addEventListener('click', function() {
+        searchBar.classList.toggle('show'); // Toggle class .show để hiển thị/ẩn input
+        pageTitle.classList.toggle('hidden'); // Toggle class .hidden để ẩn/hiển thị tiêu đề
+    });
+
 });
